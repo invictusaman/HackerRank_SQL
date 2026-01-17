46dddf570866d215a2d334f3dc398ae3b2833439
Aman Bhattarai <41770152+invictusaman@users.noreply.github.com>
2026-01-16 21:08:40 -0500

    Update RevisingSelectQueryI.sql

--- Basic_Select/RevisingSelectQueryI.sql
+++ Basic_Select/RevisingSelectQueryI.sql
@@ -1,7 +1,7 @@
 /*
 Query all columns for all American cities in the CITY table with populations larger than 100000. The CountryCode for America is USA.
 
-Full problem statement - https://www.hackerrank.com/challenges/revising-the-select-query/problem?isFullScreen=true
+Full problem statement with example - https://www.hackerrank.com/challenges/revising-the-select-query/problem?isFullScreen=true
 */
 
 select * from city
